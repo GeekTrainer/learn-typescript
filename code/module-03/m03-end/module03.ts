12e3cdb9e68bdae4e1eaafd58aa38a449e6a5f90
Jo Bader <73563912+JoB206@users.noreply.github.com>
2021-01-11 12:50:02 -0800

    Update module03.ts

--- code/module-03/m03-end/module03.ts
+++ code/module-03/m03-end/module03.ts
@@ -2,29 +2,20 @@
    Lab End  */
 
 /*  EXERCISE 1
-    TODO: Declare an interface called Loan that defines two properties, principle 
-    and interestRate, as a number. */
+    TODO: Declare the Loan interface. */
 
 interface Loan {
     principle: number,
     interestRate: number    //* Interest rate percentage (eg. 14 is 14%)
 }
 
-/*  TODO: Declare an interface called ConventionalLoan that extends Loan, and defines the 
-    additional property required for a conventional loan, numberOfMonths, as a number. */
+/*  TODO: Declare the ConventionalLoan interface. */
 
 interface ConventionalLoan extends Loan {
     numberOfMonths: number      //* Total number of months
 }
 
-/*  TODO: Replace the two parameters in the interestOnlyLoan function with an object of 
-    type Loan (for example, loanTerms: Loan) and type the return value of the function 
-    as a string. 
-    
-    Replace the parameter names in the function with properties of the Loan object. 
-    (For example, loanTerms.interestRate).
-    
-    Type the interest and payment variables as numbers.*/
+/*  TODO: Update the interestOnlyLoan function. */
 
 function interestOnlyLoan(loanTerms: Loan): string {
     // Calculates the monthly payment of an interest only loan
@@ -34,8 +25,7 @@ function interestOnlyLoan(loanTerms: Loan): string {
     return 'The interest only loan payment is ' + payment.toFixed(2);
 }
 
-/*  TODO: Update the conventionalLoan function, this time replacing the three parameters with an 
-    object of type ConventionalLoan and type the return value of the function as a string. */
+/*  TODO: Update the conventionalLoan function. */
 
 function conventionalLoan(loanTerms: ConventionalLoan): string {
     // Calculates the monthly payment of a conventional loan
