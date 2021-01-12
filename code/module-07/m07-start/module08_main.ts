1745b731a469082d36860dd159a1a4d2818eedc6
Jo Bader <73563912+JoB206@users.noreply.github.com>
2021-01-12 14:58:33 -0800

    Update module08_main.ts

--- code/module-07/m07-start/module08_main.ts
+++ code/module-07/m07-start/module08_main.ts
@@ -1,7 +1,7 @@
 /*  Module 8: Organize code in TypeScript
     Lab Start */
 
-/*  TODO: Create a new namespace called Loans. */
+/*  TODO Create a the Loans namespace. */
 
 
 interface Loan {
@@ -12,16 +12,16 @@ interface ConventionalLoan extends Loan {
     months: number      //* Total number of months
 }
 
-/*  TODO: Create a new namespace called LoanPrograms. */
+/*  TODO Create LoanPrograms namespace. */
 
 
-// Calculates the monthly payment of an interest only loan
+/*  TODO Update the calculateInterestOnlyLoanPayment function. */
 function calculateInterestOnlyLoanPayment(loanTerms: Loan): string {
     let payment: number;
     payment = loanTerms.principle * calculateInterestRate(loanTerms.interestRate);
     return 'The interest only loan payment is ' + payment.toFixed(2);
 }
-// Calculates the monthly payment of a conventional loan      
+/*  TODO Update the calculateConventionalLoanPayment function. */  
 function calculateConventionalLoanPayment(loanTerms: ConventionalLoan): string {
     let interest: number = calculateInterestRate(loanTerms.interestRate);
     let payment: number;
@@ -32,8 +32,8 @@ function calculateInterestRate (interestRate: number): number {
     let interest: number = interestRate / 1200;
     return interest
 }
-/*  TODO: Update the function calls. */
+/*  TODO Update the function calls. */
 let interestOnlyPayment = calculateInterestOnlyLoanPayment({principle: 30000, interestRate: 5});
 let conventionalLoanPayment = calculateConventionalLoanPayment({principle: 30000, interestRate: 5, months: 180});
-console.log(interestOnlyPayment);     //* Returns "The interest only loan payment is 125.00" 
+console.log(interestOnlyPayment);         //* Returns "The interest only loan payment is 125.00" 
 console.log(conventionalLoanPayment);     //* Returns "The conventional loan payment is 237.24"
