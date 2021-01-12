2881b026e53b41e2340c1c7ffb7a86408f220151
Jo Bader <73563912+JoB206@users.noreply.github.com>
2021-01-12 12:30:49 -0800

    Update module06.ts

--- code/module-06/m06-start/module06.ts
+++ code/module-06/m06-start/module06.ts
@@ -4,11 +4,11 @@
 /*  DataStore is a utility function that can store up to 10 string values in an array. 
     Rewrite the DataStore class so the array can store items of any type.
 
-    TODO: Add a type variable called T to the class declaration. */
+    TODO: Add and apply a type variable. */
 class DataStore {
-    //TODO: Add the type variable to the _data property declaration.
+
     private _data = new Array(10);
-    //TODO: Update the type of the item parameter in the AddOrUpdate function to use the type variable.
+    
     AddOrUpdate(index: number, item: string) {
         if(index >=0 && index <10) {
             this._data[index] = item;
@@ -34,7 +34,4 @@ cities.AddOrUpdate(11, "London");       // item not added
 console.log(cities.GetData(1));         // returns 'Chicago'
 console.log(cities.GetData(12));        // returns 'undefined'
 
-// TODO Test items as numbers
-
-// TODO Test items as objects
-
+// TODO Test items as numbers and objects.
