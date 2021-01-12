eb137818ee3a3c472150fe979333b87a52724ed1
Jo Bader <73563912+JoB206@users.noreply.github.com>
2021-01-12 14:15:35 -0800

    Delete module07_loans.ts

--- code/module-07/m07-end/module07_loans.ts
+++
@@ -1,8 +0,0 @@
-
-export interface Loan {
-    principle: number,
-    interestRate: number        //* Interest rate percentage (eg. 14 is 14%)
-}
-export interface ConventionalLoan extends Loan {
-    numberOfMonths: number      //* Total number of months
-}
