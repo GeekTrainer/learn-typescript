eee7c6b03d92e76a624a78c15be128557f1c42f6
Jo Bader <73563912+JoB206@users.noreply.github.com>
2021-01-12 15:01:20 -0800

    Update module08_main.ts

--- code/module-07/m07-end/module08_main.ts
+++ code/module-07/m07-end/module08_main.ts
@@ -8,8 +8,7 @@
 /// <reference path="module07_loans.ts" />
 /// <reference path="module07_loan-programs.ts" />
 
-let loanA = LoanPrograms.interestOnlyLoan({principle: 30000, interestRate: 5});
-let loanB = LoanPrograms.conventionalLoan({principle: 30000, interestRate: 5, numberOfMonths: 180});
-
-console.log(loanA);     //* Returns "The interest only loan payment is 125.00" 
-console.log(loanB);     //* Returns "The conventional loan payment is 237.24" 
+let interestOnlyPayment = calculateInterestOnlyLoanPayment({principle: 30000, interestRate: 5});
+let conventionalLoanPayment = calculateConventionalLoanPayment({principle: 30000, interestRate: 5, months: 180});
+console.log(interestOnlyPayment);         //* Returns "The interest only loan payment is 125.00" 
+console.log(conventionalLoanPayment);     //* Returns "The conventional loan payment is 237.24"
