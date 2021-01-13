5726021d2041252d9f8c873647ace02d6f0d4852
Jo Bader <73563912+JoB206@users.noreply.github.com>
2021-01-12 16:39:06 -0800

    Update module08_main.ts

--- code/module-07/m07-end/module08_main.ts
+++ code/module-07/m07-end/module08_main.ts
@@ -9,7 +9,8 @@
 /// <reference path="module08_loans.ts" />
 /// <reference path="module08_loan-programs.ts" />
 
-let interestOnlyPayment = calculateInterestOnlyLoanPayment({principle: 30000, interestRate: 5});
-let conventionalLoanPayment = calculateConventionalLoanPayment({principle: 30000, interestRate: 5, months: 180});
+/*  TODO Update the function calls. */
+let interestOnlyPayment = LoanPrograms.calculateInterestOnlyLoanPayment({principle: 30000, interestRate: 5});
+let conventionalLoanPayment = LoanPrograms.calculateConventionalLoanPayment({principle: 30000, interestRate: 5, months: 180});
 console.log(interestOnlyPayment);         //* Returns "The interest only loan payment is 125.00" 
 console.log(conventionalLoanPayment);     //* Returns "The conventional loan payment is 237.24"
